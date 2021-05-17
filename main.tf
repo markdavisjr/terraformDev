94795f22a4e1c26a684eb46da78026f0d3d077f6
Mark Davis <markalandavisii@gmail.com>
2021-05-17 19:21:46 -0400

    added instance security groups

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "aws_default_vpc" "default" {
 }
 
 resource "aws_autoscaling_group" "bar" {
-  name                      = "node-autoscaling-group-tf"
+  name                      = "node-autoscaling-group-tf2"
   max_size                  = 3
   min_size                  = 1
   health_check_grace_period = 300
@@ -14,6 +14,7 @@ resource "aws_autoscaling_group" "bar" {
   force_delete              = true
   launch_configuration      = aws_launch_configuration.as_conf.name
   vpc_zone_identifier       = ["subnet-409a292b", "subnet-de0de4a3"]
+  target_group_arns         = [aws_lb_target_group.tg.arn]
 
 
   timeouts {
@@ -28,6 +29,37 @@ resource "aws_launch_configuration" "as_conf" {
     name          = "nodeTemplate"
     image_id      = "ami-082b957f5b3409dc9"
     instance_type = "t2.micro"
+    security_groups = [aws_security_group.instance_sg.id]
+}
+
+resource "aws_security_group" "instance_sg" {
+    name = "EC2 SG"
+    description = "EC2 instance SG"
+    vpc_id = aws_default_vpc.default.id
+
+    ingress {
+        description      = "SSH for me"
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["173.49.136.170/32"]
+    }
+
+    ingress {
+        description      = "app port"
+        from_port        = 4000
+        to_port          = 4000
+        protocol         = "tcp"
+        cidr_blocks      = [aws_default_vpc.default.cidr_block]
+    }
+
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+
 }
 
 resource "aws_security_group" "lb_sg" {
@@ -40,8 +72,8 @@ resource "aws_security_group" "lb_sg" {
         from_port        = 443
         to_port          = 443
         protocol         = "tcp"
-        cidr_blocks      = [aws_vpc.main.cidr_block]
-        ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
     }
 
     ingress {
@@ -49,8 +81,8 @@ resource "aws_security_group" "lb_sg" {
         from_port        = 80
         to_port          = 80
         protocol         = "tcp"
-        cidr_blocks      = [aws_vpc.main.cidr_block]
-        ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
     }
 
 
@@ -73,7 +105,7 @@ resource "aws_lb" "lb" {
     internal           = false
     load_balancer_type = "application"
     security_groups    = [aws_security_group.lb_sg.id]
-    subnets            = ["subnet-5808ec25", "subnet-2b084767"]
+    subnets            = ["subnet-409a292b", "subnet-de0de4a3"]
 
     enable_deletion_protection = false
 
